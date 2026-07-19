fd7f00ebe42dbb2b9e9d353abdb3c2b2ef7acbcc
ALTUniversitiProekt <qq7435517@gmail.com>
2026-07-20 00:02:11 +0600

    Create App component with multiple subcomponents

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import Hero from "./components/Hero";
+import Invitation from "./components/Invitation";
+import Countdown from "./components/Countdown";
+import Location from "./components/Location";
+import RSVP from "./components/RSVP";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Hero />
+      <Invitation />
+      <Countdown />
+      <Location />
+      <RSVP />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
